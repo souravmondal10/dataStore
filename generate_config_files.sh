c1b54df1159eb7c4bf223cc90309a2cb672d0be8
sourav mondal <souravmondal10@gmail.com>
2021-06-24 08:33:35 +0000

    latest changes

--- generate_config_files.sh
+++ generate_config_files.sh
@@ -9,3 +9,4 @@ sed -i "s/mysql_database_xxx/$MYSQL_DATABASE/g" config.php
 sed -i "s/mysql_user_xxx/$MYSQL_USER/g" config.php
 sed -i "s/mysql_password_xxx/$MYSQL_PASSWORD/g" config.php
 cat config.php
+docker build -t souravmondal10/datastore .
